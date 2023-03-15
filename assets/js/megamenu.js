b14947bacaf5c450b9d3d4eb0bbf2d23293aa65f
Eiger <xl3665@nyu.edu>
2023-03-14 21:28:10 -0400

    megamenu done?

--- assets/js/megamenu.js
+++ assets/js/megamenu.js
@@ -1,7 +1,13 @@
 (function(){
     const gh_head = $("#gh-head");
     const nav_items = $(".gh-head-menu .nav-item");
+    
+    // set z-index in reverse to the html code order,
+    //      so that menu won't occlude dropdown
     nav_items.each(function(i, c) {
         $(c).css('z-index', nav_items.length-i);
     });
+
+    
+
 })()
