ea5656d0fa2efb0c3b15a1e674145cd12fbc00d1
Eiger <xl3665@nyu.edu>
2023-03-16 23:58:55 -0400

    dropdown on hover

--- assets/js/megamenu.js
+++ assets/js/megamenu.js
@@ -1,4 +1,5 @@
 (function(){
+    alert("load megamenu.js")
     const gh_head = $("#gh-head");
     const nav_items = $(".gh-head-menu .nav-item");
     
@@ -8,6 +9,28 @@
         $(c).css('z-index', nav_items.length-i);
     });
 
-    
+    // // make nav-link clickable, show/hide .dropdown-menu
+    // nav_items.each(function(i, c) {
+    //     $(".nav-link", c).click(function(e) {
+    //         console.log("click")
+    //         e.preventDefault();
+    //         e.stopPropagation();
+    //         // add or remove .show class to .dropdown-menu
+    //         $(".dropdown-menu", c).toggleClass("show");
+    //     });
+    // })
 
-})()
+    // inside hamburger menu, scroll to top on click
+
+    // $(window).resize(function() {
+    //     // alert("resize");
+
+    //     $(".nav-item .dropdown-menu").width(
+    //         $(".gh-head-menu").width()
+    //     ).css({
+    //         left: $(".gh-head-menu").offset().left
+    //     })
+
+        
+    // }).resize();
+})();
