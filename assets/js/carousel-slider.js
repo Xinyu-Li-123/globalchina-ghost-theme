2e35b249eb79a820423820b0e90dc42d7186fea0
Eiger <xl3665@nyu.edu>
2023-04-07 02:35:18 -0400

    center carousel

---
+++ assets/js/carousel-slider.js
@@ -0,0 +1,10 @@
+// (function(){
+//     // adjust carousel left to center the carousel on resize
+//     const siteMain = $('#site-main');
+//     $(window).resize(function(){
+//         var carousel = $('.carousel');
+//         console.log((2*parseFloat(siteMain.css("padding")) + siteMain.width() - carousel.width()) / 2 + 'px');
+//         carousel.css('left', 
+//             (2*parseFloat(siteMain.css("padding")) + siteMain.width() - carousel.width()) / 2 + 'px');
+//     });
+// })();
